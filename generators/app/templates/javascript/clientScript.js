c85d3a080418e1666da336f649ee71ba08c3667e
mrflippy <mrflippy@tresgeek.net>
2019-05-06 22:35:24 -0600

    Update javascript templates to work with 1.12 beta changes

--- generators/app/templates/javascript/clientScript.js
+++ generators/app/templates/javascript/clientScript.js
@@ -3,6 +3,9 @@ var clientSystem = client.registerSystem(0, 0);
 // Setup which events to listen for
 clientSystem.initialize = function () {
     // set up your listenToEvents and register client-side components here.
+
+    //register the custom pinky event
+    clientSystem.registerEventData("<%= addonNamespace %>:pinky", { narf: null });
 }
 
 let firstTick = true;
@@ -11,7 +14,17 @@ clientSystem.update = function() {
     // Any logic that needs to happen every tick on the client.
     if (firstTick) {
         firstTick = false;
-        clientSystem.broadcastEvent("minecraft:display_chat_event", "What are we going to do tonight Server?");
-        clientSystem.broadcastEvent("<%= addonNamespace %>:pinky", {narf: false});
+
+        //set up chat event data object
+        let chatEventData = clientSystem.createEventData("minecraft:display_chat_event");
+        chatEventData.data.message = "What are we going to do tonight Server?";
+
+        clientSystem.broadcastEvent("minecraft:display_chat_event", chatEventData);
+
+        //set up pink event data object
+        let pinkyEventData = clientSystem.createEventData("<%= addonNamespace %>:pinky");
+        pinkyEventData.narf = false;
+
+        clientSystem.broadcastEvent("<%= addonNamespace %>:pinky", pinkyEventData);
     }
 }
